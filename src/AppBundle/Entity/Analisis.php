0e609fe137c4539c91740338b706dcee19724b4e
CrisCep1995 <cepedacristian@outlook.com>
2018-11-20 21:06:55 -0300

    cree BD, Entidades

---
+++ src/AppBundle/Entity/Analisis.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection ;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints  as  Assert ;
+/**
+ * Analisis
+ *
+ * @ORM\Table(name="analisis")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AnalisisRepository")
+ */
+class Analisis implements \JsonSerializable
+{
+
+    /**
+    * @ ORM \ ManyToMany (targetEntity = "Paciente", mappedBy = "Analisis")
+    * @ ORM \ JoinTable (name = "Paciente_analisis")
+    */
+    private $paciente=null;
+    public function __construct()
+    {
+        $this->paciente= new ArrayCollection();
+    }
+    public function getPaciente()
+    {
+        return $this->paciente;
+    }
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Analisis
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function jsonSerialize ()
+    {
+        return [
+                    ' id ' => $ this -> getId (),
+                    ' name ' => $ this -> getName (),
+        ];
+    }
+}
+
---
+++ src/AppBundle/Entity/Paciente.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+//use ecommarg\cart\ProductInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Paciente
+ *
+ * @ORM\Table(name="paciente")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PacienteRepository")
+ */
+class Paciente //implements PacienteInterface
+{   
+    /**
+    *@ORM\ManyToMany(targetEntity="Analisis", inversedBy="Paciente")
+    *@ORM\JoinTable(name="Paciente_analisis")
+    */
+    private $categories=null;
+    public function __construct()
+    {
+        $this->categories=new ArrayCollection();
+    }
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="Age", type="integer")
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="idNumber", type="string", length=255)
+     */
+    private $idNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="idType", type="string", length=255)
+     */
+    private $idType;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Paciente
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Paciente
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Paciente
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set idNumber
+     *
+     * @param string $idNumber
+     *
+     * @return Paciente
+     */
+    public function setIdNumber($idNumber)
+    {
+        $this->idNumber = $idNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get idNumber
+     *
+     * @return string
+     */
+    public function getIdNumber()
+    {
+        return $this->idNumber;
+    }
+
+    /**
+     * Set idType
+     *
+     * @param string $idType
+     *
+     * @return Paciente
+     */
+    public function setIdType($idType)
+    {
+        $this->idType = $idType;
+
+        return $this;
+    }
+
+    /**
+     * Get idType
+     *
+     * @return string
+     */
+    public function getIdType()
+    {
+        return $this->idType;
+    }
+}
+
